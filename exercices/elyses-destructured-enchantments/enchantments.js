d3c2660845135fac523f893c9fd4a28cfb820898
SHR-Zidane <pd01wvj@eduvaud.ch>
2025-12-05 14:23:24 +0100

    feat(elyses-destructured-enchantments): Réalisation de l'exercice [60][DONE

--- exercices/elyses-destructured-enchantments/enchantments.js
+++ exercices/elyses-destructured-enchantments/enchantments.js
@@ -9,7 +9,8 @@
  * @returns {Card} the first card in the deck
  */
 export function getFirstCard(deck) {
-  throw new Error('Implement the getFirstCard function');
+  const [card] = deck;
+  return card
 }
 
 /**
@@ -20,7 +21,8 @@ export function getFirstCard(deck) {
  * @returns {Card} the second card in the deck
  */
 export function getSecondCard(deck) {
-  throw new Error('Implement the getSecondCard function');
+  const [card, card2] = deck;
+  return card2
 }
 
 /**
@@ -31,7 +33,8 @@ export function getSecondCard(deck) {
  * @returns {[Card, Card]} new deck with the 2 cards swapped
  */
 export function swapTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+  const [card, card2] = deck;
+  return [card2, card]
 }
 
 /**
@@ -42,7 +45,8 @@ export function swapTwoCards(deck) {
  * @returns {[Card, Card, Card]} new deck with the 3 cards shifted by one position
  */
 export function shiftThreeCardsAround(deck) {
-  throw new Error('Implement the discardTopCard function');
+  const [card, card2, card3] = deck;
+  return [card2, card3, card]
 }
 
 /**
@@ -54,7 +58,8 @@ export function shiftThreeCardsAround(deck) {
  */
 export function pickNamedPile(piles) {
   // 🚨 Do NOT use piles.chosen or piles.disregarded.
-  throw new Error('Implement the pickNamedPile function');
+  const {chosen} = piles
+  return chosen
 }
 
 /**
@@ -67,5 +72,7 @@ export function swapNamedPile(piles) {
   // 🪄 Don't break the magic.
   // 🚨 Do NOT use piles.chosen or piles.disregarded.
   // 🚨 Do NOT touch the next line or Elyse will accidentally reveal the trick.
+  const {chosen: disregarded, disregarded: chosen} = piles
+
   return { chosen, disregarded };
 }
