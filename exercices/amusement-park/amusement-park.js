6c0e635b013fa27d0a301a22f2092c675587bacd
SHR-Zidane <pd01wvj@eduvaud.ch>
2025-12-12 16:31:16 +0100

    feat(amusement-park): Réalisation de l'exercice [80][WIP]

--- exercices/amusement-park/amusement-park.js
+++ exercices/amusement-park/amusement-park.js
@@ -10,7 +10,7 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  return {name: name, age: age, ticketId: ticketId}
 }
 
 /**
@@ -20,7 +20,8 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Remove this line and implement the function');
+  visitor.ticketId = null;
+  return visitor
 }
 
 /**
@@ -31,7 +32,15 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  if(tickets[ticketId] === null){
+    return 'not sold'
+  }
+  else if(Boolean(tickets[ticketId])){
+    return 'sold to ' + tickets[ticketId]
+  }
+  else{
+    return 'unknown ticket id'
+  }
 }
 
 /**
@@ -43,7 +52,12 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  if(tickets[ticketId] == null){
+    return 'invalid ticket !!!'
+  }
+  else{
+    return tickets[ticketId]
+  }
 }
 
 /**
@@ -53,5 +67,10 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Remove this line and implement the function');
+  if(Boolean(visitor.gtc.version)){
+    return visitor.gtc.version
+  }
+  else{
+    return undefined
+  }
 }
