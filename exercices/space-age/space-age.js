1ae384f44db140a93e82b6cf0a31257e37e408c7
SHR-Zidane <pd01wvj@eduvaud.ch>
2026-01-04 19:07:43 +0100

    feat(space-age): commencement de l'exercice

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -4,5 +4,15 @@
 //
 
 export const age = () => {
-  throw new Error('Remove this line and implement the function');
+  const planetsAge = {
+    Mercury:  0.2408467, 
+    Venus: 0.61519726, 
+    Earth: 1.0, 
+    March: 1.8808158,
+    Jupiter: 11.862615 ,
+    Saturn: 29.447498 ,
+    Uranus: 84.016846 ,
+    Neptune: 164.79132
+  };
+  
 };
